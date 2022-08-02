f9742e9f4892c98cb34e9dfaf228d952ad465b67
Oguzcan Aygun <aygunoguz51@gmail.com>
2022-08-02 16:46:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res) {
+  res.sendFile(__dirname+"/signup.html")
+})
+app.post("/", function(req, res) {
+  var firstName= req.body.fName;
+  var secondName= req.body.sName;
+  var email= req.body.email;
+
+console.log(firstName,secondName,email);
+})
+
+app.listen(3000, function() {
+  console.log("Server is up and running on port 3000.");
+})
